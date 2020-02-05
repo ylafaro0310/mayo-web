087f58d1df45c8503d25dd6dfb3d5dfed94c005e
ylafaro0310 <y16mukurou@gmail.com>
2020-02-05 21:36:13 +0900

    Modify index pages layout

--- src/pages/works/index.js
+++ src/pages/works/index.js
@@ -9,8 +9,19 @@ export default class WorksIndexPage extends React.Component {
       <Layout>
         <section className="section">
           <div className="container">
-            <div className="content">
-              <WorksRoll />
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <section className="section">
+                  <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                    Works
+                  </h2>
+                  <div className="container">
+                    <div className="content">
+                      <WorksRoll />
+                    </div>
+                  </div>
+                </section>
+              </div>
             </div>
           </div>
         </section>
