60084954f2b36e84c5d38c623f1228249cc7e104
ylafaro0310 <y16mukurou@gmail.com>
2020-12-18 17:27:53 +0900

    Modify Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
-import './all.sass'
-import useSiteMetadata from './SiteMetadata'
-import { withPrefix } from 'gatsby'
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import './all.sass';
+import useSiteMetadata from './SiteMetadata';
+import { withPrefix } from 'gatsby';
 
 const TemplateWrapper = ({ children }) => {
-  const { title, description } = useSiteMetadata()
+  const { title, description } = useSiteMetadata();
   return (
     <div className="main-container">
       <Helmet>
@@ -49,10 +49,10 @@ const TemplateWrapper = ({ children }) => {
         />
       </Helmet>
       <Navbar />
-      <div>{children}</div>
+      <div className="has-text-centered">{children}</div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
