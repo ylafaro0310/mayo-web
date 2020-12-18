dfde6ad2dbea77696df361cfb04c664ea82f31a9
ylafaro0310 <y16mukurou@gmail.com>
2020-12-18 17:26:05 +0900

    Confiure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    globals: {
+      __PATH_PREFIX__: true,
+    },
+    extends: `react-app`,
+    rules: {
+      'indent': [
+          'error',
+          2,
+          { "SwitchCase": 1 }
+      ],
+      'quotes': [
+          'error',
+          'single'
+      ],
+      'semi': [
+          'error',
+          'always'
+      ],
+    }
+  }
